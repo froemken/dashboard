786168b42a238c4ea63c947aa53a167a2a6a90cc
Stefan Froemken <froemken@gmail.com>
2021-06-08 23:13:28 +0200

    Use google cloud functions if possible

--- app/Http/Controllers/Api/NetworkController.php
+++ app/Http/Controllers/Api/NetworkController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
+use GuzzleHttp\Client;
 use Illuminate\Routing\Controller;
 
 class NetworkController extends Controller
 {
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function getIp(): string
     {
-        return $_SERVER['REMOTE_ADDR'];
+        $remoteIp = $_SERVER['REMOTE_ADDR'];
+        $googleCloudFunctionPath = env('GOOGLE_CLOUD_FUNCTIONS_PATH');
+        if ($googleCloudFunctionPath) {
+            $remoteIp = (string)$this->client
+                ->get($googleCloudFunctionPath . 'getRemoteIpAddress')
+                ->getBody();
+        }
+        return $remoteIp;
     }
 }
